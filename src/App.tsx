bb32ffd307c85e39e96af6fa7a2076586ffe7a0e
Lyubov <klubasha2003@gmail.com>
2025-04-24 14:48:37 +0300

    PokemonComparison screen added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 // import { PokemonDetails } from "./screens/PokemonDetails"
 // import { PokemonList } from "./screens/PokemonList"
-import { PokemonFavorites } from "./screens/PokemonFavorites"
+// import { PokemonFavorites } from "./screens/PokemonFavorites"
+import { PokemonComparison } from "./screens/PokemonComparison"
 
 export const App = () => {
   return (
     <div>
       {/* <PokemonList /> */}
       {/* <PokemonDetails /> */}
-      <PokemonFavorites />
+      {/* <PokemonFavorites /> */}
+      <PokemonComparison />
     </div>
   )
 }
---
+++ src/screens/PokemonComparison/index.tsx
@@ -0,0 +1,54 @@
+import './style.css';
+import { Header } from '../../components/Header';
+import removeIcon from '../../assets/pokemonList/remove.svg';
+
+const mockData = [
+  {
+    name: 'Bulbasaur',
+    height: 7,
+    weight: 69,
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+  },
+  {
+    name: 'Charmander',
+    height: 6,
+    weight: 85,
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+  },
+];
+
+export const PokemonComparison = () => {
+  return (
+    <div className="comparison_page_container">
+      <Header />
+      <main className="comparison_main_content">
+        <div className="pokemon_comparison_list">
+          {mockData.map((pokemon, index) => (
+            <div key={index} className="comparison_card">
+              <button className="remove_button" title="Remove from comparison">
+                <img src={removeIcon} alt="Remove" />
+              </button>
+              <div className="pokemon_image_centered">
+                <img src={pokemon.image} alt={pokemon.name} />
+              </div>
+              <div className="comparison_table">
+                <div className="comparison_row">
+                  <span className="label">Name</span>
+                  <span className="value">{pokemon.name}</span>
+                </div>
+                <div className="comparison_row">
+                  <span className="label">Height</span>
+                  <span className="value">{pokemon.height}</span>
+                </div>
+                <div className="comparison_row">
+                  <span className="label">Weight</span>
+                  <span className="value">{pokemon.weight}</span>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
+};
